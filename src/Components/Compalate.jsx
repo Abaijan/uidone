4452b64bbe29ee9741b3b265d68a389f4f201616
Abaijan <fghf09647@gmail.com>
2023-07-23 14:39:11 +0600

    test commit

--- src/Components/Compalate.jsx
+++ src/Components/Compalate.jsx
@@ -4,6 +4,7 @@ import { Complate } from '../assets/Icons'
 const Compalate = () => {
   return (
     <div className='container'>
+        
         <div className="shadow-[5px_10px_15px_5px_rgba(0,0,0,0.3)] rounded-3xl mt-[147px] ml-[20%] w-[822px] h-[324px] py-[30px] flex flex-col items-center">
             {/*w-[100px] mb-[30px] mt-[30px] */}
             <Complate/>
@@ -13,5 +14,6 @@ const Compalate = () => {
     </div>
   )
 }
+
 
 export default Compalate
